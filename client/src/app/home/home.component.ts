9140399b536ee854970f310d480c77cd93c9ffd4
Bibek Gurung <bibek.gurung@cotiviti.com>
2024-03-30 18:56:59 +0545

    building user interfaces

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-home',
@@ -11,20 +9,9 @@ export class HomeComponent {
   registerMode = false;
   users: any;
 
-  constructor(private http: HttpClient, private toastr: ToastrService) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.getUsers();
-  }
-
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (response) => {
-        this.users = response;
-      },
-      error: error => this.toastr.error(error.error),
-      complete: () => console.log('Request has completed')
-    });
   }
 
   registerToggle(){
